a505ad7f9768b25d6036b708ba2ad240584a20a4
조승현 <shjo@ban-life.com>
2024-08-19 17:49:16 +0900

    퀴즈 기능 추가

--- app/controllers/api/v1/quiz_controller.rb
+++ app/controllers/api/v1/quiz_controller.rb
@@ -3,19 +3,36 @@ module Api
     class QuizController < ApplicationController
       def get_menu_problem
         menu = Menu.find(Menu.pluck(:id).sample)
-
-        menu.menu_recipes.pluck(:temperature).sample
         temperature = menu.menu_recipes.pluck(:temperature).sample
 
-        items = menu.menu_recipes.joins(:menu_recipe_item)
-                    .select(
-                      'menu_recipes.id',
-                      'menu_recipe_items.id',
-                      'menu_recipe_items.title',
-                      'menu_recipes.amount'
-                    )
-                    .where('menu_recipes.temperature = ?', temperature)
-                    .order(:order)
+        correct_recipes = menu.menu_recipes.joins(:menu_recipe_item)
+                              .select(
+                                'menu_recipes.id',
+                                'menu_recipe_items.id',
+                                'menu_recipe_items.title',
+                                'menu_recipes.amount'
+                              )
+                              .where('menu_recipes.temperature = ?', temperature)
+                              .order(:order)
+
+        items = correct_recipes.map do |recipe|
+          options = MenuRecipe
+                      .joins(:menu_recipe_item)
+                      .select(
+                        'menu_recipes.id',
+                        'menu_recipe_items.id',
+                        'menu_recipe_items.title',
+                        'menu_recipes.amount'
+                      )
+                      .where('menu_recipe_items.title != ?', recipe.title)
+                      .order("RAND()")
+                      .limit(4).to_a
+          options = options.map do |option|
+            parse_quiz_option(option, false)
+          end
+          options << parse_quiz_option(recipe, true)
+          options.shuffle
+        end
 
         render json: {
           menu: {
@@ -27,6 +44,17 @@ module Api
             temperature: temperature,
             items: items
           }
+        }
+      end
+
+      private
+
+      def parse_quiz_option(recipe, is_answer)
+        {
+          id: recipe["id"],
+          title: recipe["title"],
+          amount: recipe["amount"],
+          isAnswer: is_answer
         }
       end
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,28 +11,28 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_08_14_014722) do
-  create_table "articles", force: :cascade do |t|
+  create_table "articles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "menu_keywords", force: :cascade do |t|
+  create_table "menu_keywords", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "keyword"
     t.integer "menu_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "menu_recipe_items", force: :cascade do |t|
+  create_table "menu_recipe_items", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
   end
 
-  create_table "menu_recipes", force: :cascade do |t|
+  create_table "menu_recipes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "menu_id"
     t.integer "item_id"
     t.string "amount"
@@ -42,7 +42,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_14_014722) do
     t.string "temperature"
   end
 
-  create_table "menus", force: :cascade do |t|
+  create_table "menus", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.integer "searchCount", default: 0
     t.binary "image"

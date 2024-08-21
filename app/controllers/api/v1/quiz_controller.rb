23d50c509694aeb49a560239c6a6e4d57e289a50
조승현 <shjo@ban-life.com>
2024-08-21 17:26:34 +0900

    그룹별 퀴즈기능 추가

--- app/controllers/api/v1/quiz_controller.rb
+++ app/controllers/api/v1/quiz_controller.rb
@@ -2,7 +2,8 @@ module Api
   module V1
     class QuizController < ApplicationController
       def get_menu_problem
-        menu = Menu.find(Menu.pluck(:id).sample)
+        menu = get_quiz_menu
+
         temperature = menu.menu_recipes.pluck(:temperature).sample
 
         correct_recipes = menu.menu_recipes.joins(:menu_recipe_item)
@@ -48,6 +49,16 @@ module Api
       end
 
       private
+
+      def get_quiz_menu
+        if params['groupId'].nil?
+          return Menu.find(Menu.pluck(:id).sample)
+        end
+
+        menu_groups = MenuGroup.where({ group_id: params['groupId'] })
+        random_menu_id = menu_groups.pluck(:menu_id).sample
+        Menu.find(random_menu_id)
+      end
 
       def parse_quiz_option(recipe, is_answer)
         {
